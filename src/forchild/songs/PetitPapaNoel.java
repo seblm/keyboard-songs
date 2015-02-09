6b7cc6c5e51f07652c0f0dae2cdde3c30091caea
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-02-09 23:10:49 +0100

    add new lines for Petit Papa Noël song

--- src/forchild/songs/PetitPapaNoel.java
+++ src/forchild/songs/PetitPapaNoel.java
@@ -30,9 +30,19 @@ public class PetitPapaNoel implements Iterable<P> {
             new P(BLANCHE_POINT, MI_5), new P(NOIRE, RE_5),                                 // vir De
 
             new P(NOIRE_POINT, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, SI_4), new P(CROCHE, LA_4), // hors tu vas avoir si
-            new P(BLANCHE_POINT, SOL_4), new P(CROCHE, SOL_4), new P(CROCHE, SOL_4),                                                           // froid C'est un
+            new P(BLANCHE_POINT, SOL_4), new P(CROCHE, SOL_4), new P(CROCHE, SOL_4),                                                           // froid. C'est un
             new P(BLANCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, RE_5), new P(CROCHE, RE_5),                          // peu à cause de
-            new P(RONDE, DO_5)                                                                                                                 // moi
+            new P(RONDE, DO_5),                                                                                                                // moi.
+
+            new P(CROCHE, LA_4), new P(CROCHE, LA_4), new P(CROCHE, LA_4), new P(CROCHE, LA_4), new P(NOIRE, LA_4), new P(CROCHE, LA_4), new P(CROCHE, SI_4), // Il me tarde tant que le
+            new P(NOIRE_POINT, DO_5), new P(CROCHE, LA_4), new P(NOIRE, LA_4), new P(NOIRE, SOL_4),                                                           // jour se lève
+            new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(NOIRE, DO_5), new P(CROCHE, SI_4), new P(CROCHE, DO_5), // Pour voir si tu m'as appor
+            new P(RONDE, RE_5),                                                                                                                               // té
+
+            new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(NOIRE, MI_5_BEMOL), new P(CROCHE, RE_5), new P(CROCHE, MI_5_BEMOL), // tous les beaux joujoux que je
+            new P(NOIRE_POINT, FA_5), new P(CROCHE, RE_5), new P(NOIRE, DO_5), new P(NOIRE, SI_4_BEMOL),                                                                                          // vois en rêve
+            new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(NOIRE, FA_5), new P(CROCHE, FA_5), new P(CROCHE, FA_5),             // et que je t'ai comman
+            new P(BLANCHE_POINT, SOL_5),                                                                                                                                                          // dé.
     };
 
     @Override
