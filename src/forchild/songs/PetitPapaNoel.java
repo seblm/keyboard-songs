a67644c2743a489c2c87e35a86dc06da7bc7af2a
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-02-10 22:58:29 +0100

    Finish PetitPapaNoel song

--- src/forchild/songs/PetitPapaNoel.java
+++ src/forchild/songs/PetitPapaNoel.java
@@ -43,6 +43,29 @@ public class PetitPapaNoel implements Iterable<P> {
             new P(NOIRE_POINT, FA_5), new P(CROCHE, RE_5), new P(NOIRE, DO_5), new P(NOIRE, SI_4_BEMOL),                                                                                          // vois en rêve
             new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(CROCHE, MI_5_BEMOL), new P(NOIRE, FA_5), new P(CROCHE, FA_5), new P(CROCHE, FA_5),             // et que je t'ai comman
             new P(BLANCHE_POINT, SOL_5),                                                                                                                                                          // dé.
+            
+            new P(NOIRE, SOL_4),                                                                                                               // Pe
+            new P(NOIRE, DO_5), new P(NOIRE, DO_5), new P(NOIRE, DO_5), new P(NOIRE, RE_5),                                                    // tit papa No
+            new P(BLANCHE_POINT, DO_5), new P(CROCHE, DO_5), new P(CROCHE, RE_5),                                                              // ël, Quand tu
+            new P(NOIRE, MI_5), new P(NOIRE, MI_5), new P(NOIRE, MI_5), new P(NOIRE, FA_5),                                                    // descendra du
+            new P(BLANCHE_POINT, MI_5), new P(NOIRE, RE_5),                                                                                    // ciel, A
+            new P(NOIRE_POINT, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, SI_4), new P(CROCHE, LA_4), // vec tes jouets par mil
+
+            new P(BLANCHE_POINT, SOL_4), new P(CROCHE, SOL_4), new P(CROCHE, SOL_4),                                  // liers N'oublie
+            new P(BLANCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, SI_4), new P(CROCHE, DO_5), // pas mon petit sou
+            new P(BLANCHE_POINT, RE_5), new P(NOIRE, SOL_4),                                                          // lier Mais
+            new P(NOIRE, DO_5), new P(NOIRE, DO_5), new P(NOIRE, DO_5), new P(NOIRE, RE_5),                           // avant de par
+
+            new P(BLANCHE_POINT, DO_5), new P(CROCHE, DO_5), new P(CROCHE, RE_5),                                                              // tir, Il fau
+            new P(NOIRE, MI_5), new P(NOIRE, MI_5), new P(NOIRE, MI_5), new P(NOIRE, FA_5),                                                    // dra bien te cou
+            new P(BLANCHE_POINT, MI_5), new P(NOIRE, RE_5),                                                                                    // vir De
+            new P(NOIRE_POINT, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, SI_4), new P(CROCHE, LA_4), // hors tu vas avoir si
+
+            new P(BLANCHE_POINT, SOL_4), new P(CROCHE, SOL_4), new P(CROCHE, SOL_4),                                  // froid. C'est un
+            new P(BLANCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, DO_5), new P(CROCHE, RE_5), new P(CROCHE, RE_5), // peu à cause de
+            new P(BLANCHE_POINT, DO_5), new P(NOIRE, SOL_4),                                                          // moi. Pe
+            new P(NOIRE, LA_4), new P(NOIRE, DO_5), new P(NOIRE, RE_5), new P(NOIRE, FA_5),                           // tit papa No
+            new P(RONDE, SOL_5),                                                                                      // ël. 
     };
 
     @Override
